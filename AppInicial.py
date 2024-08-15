39bb987ce83936400f0fc86f850cd65086859b8c
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-08-15 10:06:05 -0500

    Update AppInicial.py

--- AppInicial.py
+++ AppInicial.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from PIL import Image
 
 st.title("Mi primera página")
+
 st.header("Si")
 st.write("pug")
 st.text("Hola Mundo aaaaa")
+imagePug = Image.open("pug.jpeg")
+st.imagen(imagen, caption = "Perro")
