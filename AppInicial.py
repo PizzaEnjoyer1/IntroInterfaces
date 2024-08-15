cf64d7ad51fb6af7486fdd27796e25ae60c76837
PizzaEnjoyer1 <133069709+PizzaEnjoyer1@users.noreply.github.com>
2024-08-15 10:21:23 -0500

    Update AppInicial.py

--- AppInicial.py
+++ AppInicial.py
@@ -1,13 +1,21 @@
 import streamlit as st
 from PIL import Image
 
-st.title("Mi primera página")
+st.title("Razas del mundo: pug")
 
-st.header("Si")
-st.write("pug")
-st.text("Hola Mundo aaaaa")
+st.header("Realizado por JCBG")
+st.text("Un pug")
 imagePug = Image.open("pugo.png")
 st.image(imagePug, caption = "Perro")
 
 texto = st.text_input("Escribe algo: ", "Este es mi texto")
 st.write("El texto escrito es: ", texto)
+
+col1, col2 = st.columns(2)
+
+with col1:
+  st.subheader("Este es una descripción de la raza")
+  st.write("El perro proviene de la China")
+  resp = st.checkbox("Estoy de acuerdo")
+  if resp:
+    st.write("Correcto")
